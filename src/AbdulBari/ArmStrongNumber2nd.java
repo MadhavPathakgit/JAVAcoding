58459474f4593fdcaab30c70a4765148cc3f2a5a
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-22 00:48:15 +0530

    done

---
+++ src/AbdulBari/ArmStrongNumber2nd.java
@@ -0,0 +1,36 @@
+package AbdulBari;
+
+import java.util.Scanner;
+
+public class ArmStrongNumber2nd {
+    public static void main( String[] args ) {
+        // USING POWER FUNCTION
+        System.out.println("Enter the positive  number to find the whether it is armstrong or not");
+        Scanner sc = new Scanner( System.in );
+        int n = sc.nextInt();
+        int originalNumber = n;
+
+        int digits = 0;
+        while(originalNumber>0){
+            digits++;
+            originalNumber= originalNumber/10;
+        }
+        originalNumber = n;
+        System.out.println("the number odf digits are "+digits);
+        int sum = 0;
+        while(originalNumber>0){
+            int remainder = originalNumber%10;
+            sum = sum+(int)Math.pow( remainder,digits );
+            originalNumber=    originalNumber/10;
+
+
+        }
+
+        if(n==sum){
+            System.out.println("is armstrong");
+        }
+        else{
+            System.out.println("not a armstrong number");
+        }
+    }
+}
