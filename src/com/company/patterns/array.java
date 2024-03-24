ce352c4a2415ec5906a280944fa19de79dd14ea6
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-24 23:42:47 +0530

    done

--- src/com/company/patterns/array.java
+++ src/com/company/patterns/array.java
@@ -3,6 +3,7 @@ package com.company.patterns;
 public class array {
     public static void main( String[] args ) {
         int[]arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println(arr.length);
 
     }
 }
