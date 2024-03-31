0ae1a10713afb90e0096507b1eb4e46f61f4a13e
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 13:27:08 +0530

    done

--- src/com/company/patterns/SecondLargestElement.java
+++ src/com/company/patterns/SecondLargestElement.java
@@ -3,8 +3,8 @@ package com.company.patterns;
 public class SecondLargestElement {
     public static void main( String[] args ) {
         int [] arr= {1,2,3,4,5,6,5,8,9,1,0};
-        int largest= arr[0];
-        int SecondLargest= arr[0];
+        int largest= Integer.MIN_VALUE;
+        int SecondLargest= Integer.MIN_VALUE;
         for(int i =0;i< arr.length;i++){
             if(largest<arr[ i ]){
                 SecondLargest= largest;
