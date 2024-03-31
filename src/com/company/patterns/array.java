413ab501d467fed057e57f472e620cbac295450d
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 12:22:51 +0530

    done

--- src/com/company/patterns/array.java
+++ src/com/company/patterns/array.java
@@ -4,6 +4,10 @@ public class array {
     public static void main( String[] args ) {
         int[]arr = {1,2,3,4,5,6,7,8,9};
         System.out.println(arr.length);
+        System.out.println();
+        for( int x:arr){
+            System.out.print(x+" ");
+        }
 
     }
 }
