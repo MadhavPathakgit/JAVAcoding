c629307707480b6001a15d2f85afb4b15b6eb36f
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 22:16:41 +0530

    done

--- src/AbdulBari/InsertingElementInArray.java
+++ src/AbdulBari/InsertingElementInArray.java
@@ -6,31 +6,31 @@ public class InsertingElementInArray {
     public static void main( String[] args ) {
         //insert element at an index;
 
-        int [] arr = new int[10];
-        arr[0]= 1;
-        arr[1]=2;
-        arr[2]= 3;
-        arr[3]= 4;
-        arr[4]= 4;
-        arr[5]= 4;
-        arr[6]= 4;
+        int[] arr = new int[ 10 ];
+        arr[ 0 ] = 1;
+        arr[ 1 ] = 2;
+        arr[ 2 ] = 3;
+        arr[ 3 ] = 4;
+        arr[ 4 ] = 4;
+        arr[ 5 ] = 4;
+        arr[ 6 ] = 4;
         int n = 0;
-        int k = 0;
-        while(arr[k]!=0){
+
+        while ( arr[ n ] != 0 ) {
             n++;
-            k++;
+//            k++;
         }
 
 //        System.out.println(n);
-        System.out.println("enter the index you want to enter the element");
+        System.out.println( "enter the index you want to enter the element" );
         Scanner sc = new Scanner( System.in );
         int index = sc.nextInt();
-        System.out.println("enter the element you want to enter");
+        System.out.println( "enter the element you want to enter" );
         int element = sc.nextInt();
-        for(int i =n-1;i>index;i--){
-            arr[i]= arr[i-1];
+        for ( int i = n - 1 ; i > index ; i-- ) {
+            arr[ i ] = arr[ i - 1 ];
         }
-       arr[index] = element;
+        arr[ index ] = element;
         for ( int j : arr ) {
             System.out.print( j + " " );
         }
--- src/com/company/patterns/diamondPattern.java
+++ src/com/company/patterns/diamondPattern.java
@@ -13,21 +13,21 @@ public class diamondPattern {
 
             System.out.println();
 
-        }
+        } 
         //2nd half
-        for(int i = rows;i>=1;i--)
-        {
-            for (int j = rows-i; j>= 1; j--) {
-                System.out.print("   ");
-            }
-                for(int j= 1;j<= (2*i)-1;j++){
-                System.out.print("*  ");
-            }
-
-            System.out.println();
-
-        }
-
+//        for(int i = rows;i>=1;i--)
+//        {
+//            for (int j = rows-i; j>= 1; j--) {
+//                System.out.print("   ");
+//            }
+//                for(int j= 1;j<= (2*i)-1;j++){
+//                System.out.print("*  ");
+//            }
+//
+//            System.out.println();
+//
+//        }
+//
     }
     public static void main(String[] args) {
         diamond(4);
