e78d81832fabd6f2ebed40465f203bf0df450b7f
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 13:51:58 +0530

    done

---
+++ src/AbdulBari/RotatingAnArray.java
@@ -0,0 +1,18 @@
+package AbdulBari;
+
+public class RotatingAnArray {
+    public static void main( String[] args ) {
+         int [] arr = {5,9,6,10,2,7,3,5,4,2};
+         //Left Rotating an array;
+        int temp = arr[0];
+
+        for(int i = 1;i< arr.length;i++) {
+            arr[ i-1] = arr[ i ];
+        }
+        arr[ arr.length-1]= temp;
+
+        for ( int j : arr ) {
+            System.out.print( j + " " );
+        }
+    }
+}
--- src/com/company/patterns/MaxElementArray.java
+++ src/com/company/patterns/MaxElementArray.java
@@ -1,5 +1,6 @@
 package com.company.patterns;
 
+
 public class MaxElementArray {
     public static void main( String[] args ) {
         int[]arr= {1,2,3,4,5,6,7,8,9};
