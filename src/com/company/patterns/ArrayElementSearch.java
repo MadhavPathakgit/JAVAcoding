3c8d2c3c5bb0dce037ffd61a2974ae4bb08fe200
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 12:44:08 +0530

    done

---
+++ src/com/company/patterns/ArrayElementSearch.java
@@ -0,0 +1,23 @@
+package com.company.patterns;
+import java.util.*;
+
+public class ArrayElementSearch {
+    public static void main( String[] args ) {
+        int[] arr = { 1 , 2 , 3 , 4 , 5 , 6 };
+
+        Scanner sc = new Scanner( System.in );
+        System.out.println( "enter the element you want to search" );
+
+        int x = sc.nextInt();
+        for ( int i = 0 ; i < arr.length ; i++ ) {
+            if ( arr[ i ] == x ) {
+                System.out.println( i );
+                System.exit(0);
+            }
+
+        }
+        System.out.println( "Element not Found" );
+    }
+}
+
+
