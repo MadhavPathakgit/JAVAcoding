aece61b502a42a923cc2da18913211000d599d86
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-03-31 12:55:44 +0530

    done

---
+++ src/com/company/patterns/MaxElementArray.java
@@ -0,0 +1,14 @@
+package com.company.patterns;
+
+public class MaxElementArray {
+    public static void main( String[] args ) {
+        int[]arr= {1,2,3,4,5,6,7,8,9};
+        int largest = Integer.MIN_VALUE;
+        for ( int j : arr ) {
+            if ( largest < j ) {
+                largest = j;
+            }
+        }
+        System.out.println(largest);
+    }
+}
