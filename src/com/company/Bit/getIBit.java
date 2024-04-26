a265547c10226727edba2943b03f700da871ec8a
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-04-26 14:43:17 +0530

    done

--- src/com/company/Bit/getIBit.java
+++ src/com/company/Bit/getIBit.java
@@ -1,9 +1,13 @@
 package com.company.Bit;
 
+
+
 public class getIBit {
     public static void main( String[] args ) {
         System.out.println(getBitt( 10,3));
         System.out.println(SetBit( 10,2 ));
+        System.out.println(clearBitIth( 10,1 ));
+        System.out.println(Update( 10,2,1));
     }
     public static int getBitt(int n,int i){
         int bitMask = 1<<i;
@@ -17,5 +21,22 @@ public class getIBit {
     public static int SetBit(int n,int i){
         int BitMask = 1<<i;
       return n|BitMask;
+    }
+    public static int clearBitIth(int n,int i){
+        int bitMask = ~(1<<i);
+        return n&bitMask;
+    }
+    public static int Update(int n , int i,int newBit){
+//        if(newBit==0){
+//            return clearBitIth( n,i );
+//
+//        }
+//        else{
+//            return SetBit( n,i );
+//        }
+        n =  clearBitIth( n,i );
+        int BitMAsk = newBit<<i;
+        return n | BitMAsk;
+
     }
 }
