b0a22d35b8a6565bcc63b9dec7bfd235d47ab826
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-07-09 01:23:09 +0530

    done

---
+++ src/com/company/Hashmap/TreeHashset.java
@@ -0,0 +1,14 @@
+package com.company.Hashmap;
+
+import java.util.TreeSet;
+
+public class TreeHashset {
+    public static void main( String[] args ) {
+        TreeSet<Integer> ts = new TreeSet<>();
+        ts.add( 4 );
+        ts.add( 9 );
+        ts.add( 7 );
+        ts.add(1);
+        System.out.println(ts);
+    }
+}
