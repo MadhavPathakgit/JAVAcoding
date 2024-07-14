e70408158b77a5ecc4449c345ae7cb1f22b65f7f
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-07-14 10:21:16 +0530

    done

--- src/arraysPractice/StringCountLowerCase.java
+++ src/arraysPractice/StringCountLowerCase.java
@@ -4,13 +4,16 @@ public class StringCountLowerCase {
     public static void main( String[] args ) {
         String str = "MadHavaeiou Pathak";
         int count = 0;
-        for(int i = 0;i<str.length();i++){
-//            Character s =
-            if(str.charAt(i)=='a'|| str.charAt( i )=='e'|| str.charAt( i )=='i'|| str.charAt( i )=='o'|| str.charAt( i )=='u'){
+        for ( int i = 0 ; i < str.length() ; i++ ) {
+            if ( str.charAt( i ) == 'a' || str.charAt( i ) == 'e' || str.charAt( i ) == 'i' || str.charAt( i ) == 'o' || str.charAt( i ) == 'u' ) {
                 count++;
             }
         }
-        System.out.println(count);
+        System.out.println( count );
+
 
     }
+
+
+
 }
