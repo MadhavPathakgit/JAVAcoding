4253be99068fb81692459a291fc72652592062e2
MadhavPathakgit <madhavpathak75557@gmail.com>
2024-08-01 23:22:07 +0530

    done

--- src/com/company/Recursionn/optimisedPower.java
+++ src/com/company/Recursionn/optimisedPower.java
@@ -2,17 +2,18 @@ package com.company.Recursionn;
 
 public class optimisedPower {
     public static int opPower(int x, int n){
+//        O(logn);
         if(n==0){
             return 1;
         }
+        int half = opPower( x,n/2 );
         if(n%2==0){
-            return opPower( x,n/2 )*opPower( x,n/2 );
+            return half*half;
         }
-        return  x*opPower( x,n/2 )*opPower( x,n/2 );
-
+        return x*half*half;
     }
     public static void main( String[] args ) {
-        System.out.println(opPower( 2,5 ));
+        System.out.println(opPower( 2,10));
 
     }
 }
